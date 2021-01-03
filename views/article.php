f4059fd2780730b380f4578f462ed6d81a94b719
mmoga <matt.moga@gmail.com>
2021-01-03 18:17:25 -0500

    :art: Styled the articles

--- views/article.php
+++ views/article.php
@@ -13,16 +13,24 @@
     $article_img = $article->urlToImage;
     $placeholder_img = 'https://via.placeholder.com/350x250';
     ?>
-        <article class="wp-news-search-articles__article">
-            <div class="img__wrapper">
-                <img src="<?php echo $article_img ?: $placeholder_img; ?>" alt="">
-            </div>
-            <a href="<?php echo $article->url; ?>">
-                <h3>
-                    <?php echo $article->title; ?>
-                </h3>
-            </a>
-        </article>
-        <?php endforeach;?>
+	        <div class="wp-news-search-articles__block">
+	            <article class="wp-news-search-articles-article">
+	                <div class="img__wrapper">
+	                    <img src="<?php echo $article_img ?: $placeholder_img; ?>" alt="" loading="lazy" width="1500"
+	                        height="1368">
+	                </div>
+	                <div class="article-content">
+	                    <a href="<?php echo $article->url; ?>">
+	                        <h2 class="article-title">
+	                            <?php echo $article->title; ?>
+	                        </h2>
+	                    </a>
+	                    <p class="article-description">
+	                        <?php echo $article->description; ?>
+	                    </p>
+	                </div>
+	            </article>
+	        </div>
+	        <?php endforeach;?>
     </div>
 </div>
