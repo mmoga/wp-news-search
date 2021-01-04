630166c5876bbd4074ea87f157dcf4cd90365702
mmoga <matt.moga@gmail.com>
2021-01-03 19:26:36 -0500

    :art: Styled the form

--- views/form.php
+++ views/form.php
@@ -8,12 +8,13 @@
 ?>
 
 <form id="wp-news-search__form">
-    <label for="news-keyword">
-        Search by keywords
+    <label for="news-keyword">Search by keywords</label>
+    <div class="form__wrapper">
         <input type="text" id="news-keyword" name="news-keyword" placeholder="<?php echo __('Enter keywords') ?>"
             required>
-    </label>
-    <button id="wp-news-search__submit"><?php echo __('Search for news', 'wp-news-search') ?></button>
+
+        <button id="wp-news-search__submit"><?php echo __('Search for news', 'wp-news-search') ?></button>
+    </div>
 </form>
 
 <div id="wp-news-search-results"></div>
