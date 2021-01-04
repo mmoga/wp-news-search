819ffe2c248c8f52e825757a3bbf117319a27e81
mmoga <matt.moga@gmail.com>
2021-01-03 22:09:18 -0500

    :wink: Made the placeholder better

--- views/article.php
+++ views/article.php
@@ -11,13 +11,13 @@
     <div class="wp-news-search-articles__wrapper">
         <?php foreach ($articles as $article):
     $article_img = $article->urlToImage;
-    $placeholder_img = 'https://via.placeholder.com/350x250';
+    $placeholder_img = 'https://via.placeholder.com/900x600?text=No+article+image';
     ?>
 	        <div class="wp-news-search-articles__block">
 	            <article class="wp-news-search-articles-article">
 	                <div class="img__wrapper">
-	                    <img src="<?php echo $article_img ?: $placeholder_img; ?>" alt="" loading="lazy" width="1500"
-	                        height="1368">
+	                    <img src="<?php echo $article_img ?: $placeholder_img; ?>" alt="" loading="lazy" width="900"
+	                        height="600">
 	                </div>
 	                <div class="article-content">
 	                    <a href="<?php echo $article->url; ?>">
