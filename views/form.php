4e9352f30ee5cd3d5dae65b2faa5fb3b0a5dda4d
mmoga <matt.moga@gmail.com>
2021-01-03 21:17:50 -0500

    Changed placeholder

--- views/form.php
+++ views/form.php
@@ -10,7 +10,7 @@
 <form id="wp-news-search__form">
     <label for="news-keyword">Search by keywords</label>
     <div class="form__wrapper">
-        <input type="text" id="news-keyword" name="news-keyword" placeholder="<?php echo __('Enter keywords') ?>"
+        <input type="text" id="news-keyword" name="news-keyword" placeholder="<?php echo __('e.g. captain crunch') ?>"
             required>
 
         <button id="wp-news-search__submit"><?php echo __('Search for news', 'wp-news-search') ?></button>
